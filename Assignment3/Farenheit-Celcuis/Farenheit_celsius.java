286621bb8a0c0f2d1bb30b0ff5dccf392e3a8455
Uvashri2106 <97533128+Uvashri2106@users.noreply.github.com>
2022-01-20 21:39:22 +0530

    Add files via upload

---
+++ Assignment3/Farenheit-Celcuis/Farenheit_celsius.java
@@ -0,0 +1,9 @@
+class Farenheit_celsius
+{
+public static void main(String args[])
+{
+int F=212,C;
+C=(F-32)*5/9;
+System.out.println(C);
+}
+}
---
+++ Assignment3/Multiplication table/Multiplication_table.java
@@ -0,0 +1,9 @@
+class Multiplication_table
+{
+public static void main(String args[])
+{
+int x=8,i;
+for(i=1;i<=10;i++)
+System.out.println(x+"*"+i+"="+x*i);
+}
+}
