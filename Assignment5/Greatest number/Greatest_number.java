8a871cc21d9dd2a9c13ec1f382a960e218b3bb48
Uvashri2106 <97533128+Uvashri2106@users.noreply.github.com>
2022-01-20 21:45:15 +0530

    Add files via upload

---
+++ Assignment5/Greatest number/Greatest_number.java
@@ -0,0 +1,19 @@
+public class Greatest_number
+{
+public static void main(String args[])
+{
+int a=25,b=10,c=30;
+if(a>b&&a>c)
+{
+System.out.println("A is greatest");
+}
+else if(b>a&&b>c)
+{
+System.out.println("B is greatest");
+}
+else if(c>a&&c>b)
+{
+System.out.println("C is greatest");
+}
+}
+}
